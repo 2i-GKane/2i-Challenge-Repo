5a75565ed1f258ec36f0e2f6ced87061c374f8b4
2i-GKane <gabriel.kane@2itesting.com>
2022-06-24 21:25:29 +0100

    Enable page routing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React from "react";
-import items from "./config.json"
+import { Routes, Route } from 'react-router-dom';
 
-import { useState } from "react";
-
-import SearchBar from "./components/SearchBar";
-import Card from "./components/Card";
-import "./styling.css";
+import HomePage from "./components/HomePage";
+import EngineersPage from "./components/EngineersPage";
 
 function App() {
-  const[searchStr, setSearchStr] = useState("");
-  let matchingSubmissions = items;
-
-  if(searchStr !== "") {
-    matchingSubmissions = [];
-    items.map((item) => {
-      let projectName = item.title.toLowerCase();
-      let projectCreator = item.creator.toLowerCase();
-
-      let search = searchStr.toLowerCase();
-
-      if(projectName.includes(search) || projectCreator.includes(search)) matchingSubmissions.push(item);
-    })
-  }
-
-  let cardID = 0;
   return (
-    <div>
-      <div className="page-title">
-        <h1>2i Coding Challenge Submissions</h1>
-        <h2>Maintained by: Gabriel & Craig</h2>
-      </div>
-
-      <SearchBar updateSearch={setSearchStr}/>
-
-      <div className="container">
-      {matchingSubmissions.map((item) => {
-        cardID++;
-
-        let title = item.title,
-        creator = item.creator,
-        mainTechnology = item["main-technology"],
-        thumnail = item.thumbnail,
-        projectLink = item['project-link'],
-        projectGH = item['project-gh'];
-
-        return <Card key={cardID} title={title} mainTechnology={mainTechnology} creator={creator} thumbnail={thumnail} projectLink={projectLink} projectGH={projectGH}/>
-      })}
-    </div>
-    </div>
-
-    
+    <Routes>
+      <Route path="2i-Challenge-Repo/" element={<HomePage />}></Route>
+    </Routes>    
   );
 }
 
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import items from "../config.json"
+
+import { useState } from "react";
+
+import SearchBar from "./SearchBar";
+import Card from "./Card";
+import "../styling.css";
+
+const HomePage = () => {
+    const[searchStr, setSearchStr] = useState("");
+    let matchingSubmissions = items;
+
+    console.log("home page")
+  
+    if(searchStr !== "") {
+      matchingSubmissions = [];
+      items.map((item) => {
+        let projectName = item.title.toLowerCase();
+        let projectCreator = item.creator.toLowerCase();
+  
+        let search = searchStr.toLowerCase();
+  
+        if(projectName.includes(search) || projectCreator.includes(search)) matchingSubmissions.push(item);
+      })
+    }
+  
+    let cardID = 0;
+    return (
+        <div>
+          <div className="page-title">
+            <h1>2i Coding Challenge Submissions</h1>
+            <h2>Maintained by: Gabriel & Craig</h2>
+          </div>
+
+          <SearchBar updateSearch={setSearchStr}/>
+          <div className="homepage-links">
+              <button className="btn-engineers">Engineers</button>
+          </div>
+          
+          <div className="container">
+          {matchingSubmissions.map((item) => {
+            cardID++;
+
+            let title = item.title,
+            creator = item.creator,
+            mainTechnology = item["main-technology"],
+            thumnail = item.thumbnail,
+            projectLink = item['project-link'],
+            projectGH = item['project-gh'];
+
+            return <Card key={cardID} title={title} mainTechnology={mainTechnology} creator={creator} thumbnail={thumnail} projectLink={projectLink} projectGH={projectGH}/>
+          })}
+        </div>
+      </div>
+    )
+}
+
+export default HomePage;
