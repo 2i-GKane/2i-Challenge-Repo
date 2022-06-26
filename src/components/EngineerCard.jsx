badfc47037e6ac7c32272e10a3a8e61bda3a6586
2i-GKane <gabriel.kane@2itesting.com>
2022-06-26 16:12:04 +0100

    Fix toggling profile view when clicking social links

--- src/components/EngineerCard.jsx
+++ src/components/EngineerCard.jsx
@@ -7,11 +7,11 @@ import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 const EngineerCard = ({ name, role, projectCount, profilePicture, github, linkedin, onClick}) => {
     return (
         <div>
-            <div className="eng-card" onClick={onClick}>
-                <div className="eng-card--thumbnail">
+            <div className="eng-card">
+                <div className="eng-card--thumbnail" onClick={onClick}>
                     <img src={require(`./images/engineers/${profilePicture}`)} />
                 </div>
-                <div className="eng-card--body">
+                <div className="eng-card--body" onClick={onClick}>
                   <h1>{name}</h1>
                   <h2>{role}</h2>
                   <h3>Submissions: {projectCount}</h3>
